a6bca05adf0a6bc1668b517622d2142f46ad9449
jygga <jygga@DESKTOP-I8EBPFD>
2022-03-20 21:45:06 +0300

    Добавьте файлы проекта.

---
+++ WebAppJokes/Controllers/JokesController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAppJokes.Data;
+using WebAppJokes.Models;
+using ApplicationDbContext = WebAppJokes.Data.ApplicationDbContext;
+
+namespace WebAppJokes.Controllers
+{
+    public class JokesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Jokes
+        public ActionResult Index()
+        {
+            return View(db.Jokes.ToList());
+        }
+
+        // GET: Jokes/ShowSearchForm
+        public ActionResult ShowSearchForm()
+        {
+            return View();
+        }
+
+        // POST: Jokes/ShowSearchResult
+        public ActionResult ShowSearchResults(string SearchPhrase)
+        {
+            return View("Index", db.Jokes.Where(j => j.JokeQuestion.Contains
+                (SearchPhrase)).ToList());
+        }
+
+        // GET: Jokes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Joke joke = db.Jokes.Find(id);
+            if (joke == null)
+            {
+                return HttpNotFound();
+            }
+            return View(joke);
+        }
+
+        [Authorize]
+        // GET: Jokes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jokes/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. 
+        // Дополнительные сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,JokeQuestion,JokeAnswer")] Joke joke)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Jokes.Add(joke);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(joke);
+        }
+
+        // GET: Jokes/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Joke joke = db.Jokes.Find(id);
+            if (joke == null)
+            {
+                return HttpNotFound();
+            }
+            return View(joke);
+        }
+
+        // POST: Jokes/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. 
+        // Дополнительные сведения см. в статье https://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,JokeQuestion,JokeAnswer")] Joke joke)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(joke).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(joke);
+        }
+
+        // GET: Jokes/Delete/5
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Joke joke = db.Jokes.Find(id);
+            if (joke == null)
+            {
+                return HttpNotFound();
+            }
+            return View(joke);
+        }
+
+        // POST: Jokes/Delete/5
+        [Authorize]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Joke joke = db.Jokes.Find(id);
+            db.Jokes.Remove(joke);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebAppJokes/Models/AccountViewModels.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppJokes.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "E-Mail")]
+        public string Email { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+
+    public class SendCodeViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+        public string ReturnUrl { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Provider { get; set; }
+
+        [Required]
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+        public string ReturnUrl { get; set; }
+
+        [Display(Name = "Remember browser?")]
+        public bool RememberBrowser { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "E-Mail")]
+        public string Email { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "E-Mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-Mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Password {0} need to requier {2} symbols.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Password again")]
+        [Compare("Password", ErrorMessage = "Passwords are different.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-Mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Password {0} need to requier {2} symbols.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Password again")]
+        [Compare("Password", ErrorMessage = "Passwords are different.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-Mail")]
+        public string Email { get; set; }
+    }
+}
